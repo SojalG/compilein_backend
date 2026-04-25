1965cad2cc1302e2a6bf989acdb3e1482f07aa9f
Deepak Rathaur <rathaurdeepak312@gmail.com>
2026-04-25 12:23:19 +0530

    app_listen

--- server.js
+++ server.js
@@ -21,4 +21,5 @@ app.use(express.json());
 app.use("/api/auth", authRoutes);
 app.use("/api/find", authRoutes);
 
-app.listen(5000, () => console.log("Server running on port 5000"));
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
